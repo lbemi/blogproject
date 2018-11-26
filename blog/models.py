99ac332829138dc5077bfe66a8b1d710e195b15c
wjl <934366858@qq.com>
2018-11-26 17:40:52 +0800

    add detail.html and fix some bug

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -22,3 +24,6 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     def __str__(self):
         return self.title
+
+    def get_absoulte_url(self):
+        return reverse('blog:detail', kwargs={'pk':self.pk})
